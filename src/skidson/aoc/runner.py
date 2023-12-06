051d3ad346b10f4c6718892a7cdf363dc085e89d
Stephen Kidson <stephenkidson@gmail.com>
2023-12-05 22:41:25 -0800

    Moved resources outside of src

--- src/skidson/aoc/runner.py
+++ src/skidson/aoc/runner.py
@@ -14,7 +14,7 @@ log = logging.getLogger(__name__)
 @click.argument('date', type=click.DateTime(formats=['%Y-%m-%d']))
 @click.option('--filename', default=INPUT_FILE)
 def run(date: datetime.date, filename: str):
-    input_text = files(f"skidson.aoc.resources.{date.strftime('%Y.%m.%d')}").joinpath(filename).read_text()
+    input_text = files(f"resources.{date.strftime('%Y.%m.%d')}").joinpath(filename).read_text()
 
     module = importlib.import_module(f"skidson.aoc.solve.{date.strftime('y%Ym%md%d')}")
     print(module.solve(input_text))
