77715e5d15446c2f5e7ba53ed1eea5709a939cf2
Stephen Kidson <stephenkidson@gmail.com>
2023-12-06 19:17:59 -0800

    Day 5 - Improved performance of alamnac parser

--- src/skidson/aoc/solve/y2023m12d05.py
+++ src/skidson/aoc/solve/y2023m12d05.py
@@ -1,13 +1,32 @@
-from typing import List, Dict
+import sys
+from typing import List, Dict, Optional
+import logging
+
+log_handler = logging.StreamHandler(sys.stdout)
+log_handler.setFormatter(logging.Formatter("[%(asctime)s] %(name)s:%(levelname)s - %(message)s"))
+log = logging.getLogger(__name__)
+log.addHandler(log_handler)
+log.setLevel(logging.INFO)
 
 START_KEY = 'seed'
 END_KEY = 'location'
 
 
+class Range:
+    def __init__(self, source_start: int, dest_start: int, length: int):
+        self.source_start = source_start
+        self.dest_start = dest_start
+        self.length = length
+
+    def find(self, key: int) -> Optional[int]:
+        if key < self.source_start or key >= self.source_start + self.length:
+            return None
+        return self.dest_start + (key - self.source_start)
+
 def solve(input_text: str) -> Dict[str, any]:
     lines = input_text.splitlines()
     seeds = lines[0][7:].split()
-    maps: Dict[str, Dict[str, Dict[int, int]]] = {}
+    maps: Dict[str, Dict[str, List[Range]]] = {}
 
     # FIXME while probably technically correct, building the full map like this isn't performant and doesn't finish in
     #  time. We need to use a custom data structure that checks the range instead
@@ -19,22 +38,27 @@ def solve(input_text: str) -> Dict[str, any]:
             continue
 
         # get line up until " map:"
+        log.info(f'Parsing {lines[i][:-1]}...')
         map_name = lines[i][:-5].split('-to-')
         source = map_name[0]
         dest = map_name[1]
         i += 1
         while i < len(lines) and lines[i]:
+            log.debug(f'Parsing range {lines[i]}...')
             dest_source_range = lines[i].split()
 
-            for j in range(0, int(dest_source_range[2])):
-                if not maps.get(source):
-                    maps[source] = {}
-                if not maps[source].get(dest):
-                    maps[source][dest] = {}
-                maps[source][dest][int(dest_source_range[1]) + j] = int(dest_source_range[0]) + j
+            if not maps.get(source):
+                maps[source] = {}
+            if not maps[source].get(dest):
+                maps[source][dest] = []
+
+            maps[source][dest].append(
+                Range(int(dest_source_range[1]), int(dest_source_range[0]), int(dest_source_range[2]))
+            )
 
             i += 1
 
+    log.info('Find closest location...')
     end_to_start = {}
     for seed in seeds:
         key = START_KEY
@@ -43,7 +67,14 @@ def solve(input_text: str) -> Dict[str, any]:
             # TODO this assumes only one child key - i.e. a tree where only the parents of leaf nodes can have
             #  more than one child
             subkey = next(iter(maps[key].keys()))
-            value = maps[key][subkey].get(value, value)
+            match = None
+            for r in maps[key][subkey]:
+                match = r.find(value)
+                if match is not None:
+                    break
+
+            if match is not None:
+                value = match
             key = subkey
         end_to_start[value] = seed
 
